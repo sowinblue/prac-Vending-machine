8e645d938f0b915acc6a8b038cdbcc51ca24269e
송우인 <sowintildeblue@gmail.com>
2026-01-09 14:54:11 +0900

    함수 수정

--- manage.py
+++ manage.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     print("선택된 상품:", item)
 
     print("\n===물건 고르기===")
-    machine.pro_select(self)
+    machine.pro_select()
 
     print("\n===머니슬록===")
-    machine.money_slot(money)
+    machine.money_slot()
