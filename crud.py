fd046000679a3e43367241a3580803f16aa58601
송우인 <sowintildeblue@gmail.com>
2026-01-11 14:33:31 +0900

    CRUD 뼈대

--- crud.py
+++ crud.py
@@ -2,4 +2,35 @@
 
 class SuggestionBoard():
     def __init__(self):
-        self
+        self.comments = []
+
+    
+    def b_create(self):
+        comment = input("건의 사항")
+        self.comments.append(comment)
+        print("건의 사항이 등록되었습니다.")
+
+
+    def b_read(self):
+        if not self.comments:
+            print("등록된 건의 사항이 없습니다.")
+            return
+
+        print("등록된 건의 사항")
+        for i, comment in enumerate(self.comments, start=1):
+            print(f"{i}. {comment}")
+    
+
+    def b_update(self):
+        number = int(input("수정하고 싶은 건의 사항 번호"))
+        new_comment = input("건의사항 수정 내용")
+        self.comments[number] = new_comment
+        return
+    
+
+    def b_delete(self):
+        number = int(input("지우고 싶은 건의 사항 번호"))
+        del self.comments[number]
+    
+
+    
